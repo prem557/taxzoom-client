1edaf58d21b5a6edf8a939f98b57543540c2b55a
Prem Anurag <premanurag525@gmail.com>
2025-08-03 22:29:43 +0530

    updated others

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/taxzoom-client/',  // 👈 this is now correctly placed
-  plugins: [react()],
-})
+  base: '/taxzoom-client/', 
+   plugins: [react()],
+});
